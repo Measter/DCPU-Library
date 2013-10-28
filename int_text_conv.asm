d5c2707fb50dbea489635830f889d5ace4c6ca81
Stuart Haidon <shaidon@serayen.com>
2013-10-28 09:09:52 +0000

    Changed function to support any base.

--- int_text_conv.asm
+++ int_text_conv.asm
@@ -4,115 +4,82 @@
 ; Date:		04/09/13
 ; -----------------------
 
-; Converts a value to decimal ASCII output.
+; Converts a value to ASCII output.
 ; Maximum output is 5 digits.
 ; Output is 0-terminated.
 ; Input
-; 	SP+1 : Buffer address to store output.
-;	SP+0 : Value to convert.
-:int_dec_conv
+; 	SP+2 : Buffer address to store output.
+;	SP+1 : Value to convert.
+; 	SP+0 : Base to convert to. Must be between 2 and 36.
+:int_text_conv
 	set push, z
 	set z, sp
 	add z, 2
 	set push, a
 	set push, b
+	set push, c
 	set push, i
+
+	ifl [z+0], 2
+		set pc, .exit
+	ifg [z+0], 36
+		set pc, .exit
 	
-	set a, [z]
-	set z, [z+1]
+	set c, [z+0]
+	set a, [z+1]
+	set z, [z+2]
+	; C = Base of output.
 	; A = Value to convert.
 	; Z = Buffer address.
 
-	set i, 1
+	set i, 0
 	; I = Number of digits.
-	ifg a, 9
-		set i, 2
-	ifg a, 99
-		set i, 3
-	ifg a, 999
-		set i, 4
-	ifg a, 9999
-		set i, 5
-	add z, i
-	; Z = Right most digit address.
-	; Zero-terminate string.
-	set [z], 0
-
 	:.conv_loop_start
-		sub i, 1
-		sub z, 1
-
 		set b, a
-		mod b, 10
+		mod b, c 	; Get right-most digit.
+		div a, c 	; Remove right-most digit.
 
 		; Output ASCII value.
-		set [z], 0x30
+		ifg b, 9
+			set pc, .ten_plus
+
+		:.ten_sub
+		set [z], 0x30 	; Digit 0
 		add [z], b
-	
-		div a, 10
-		ifg i, 0
+		set pc, .continue
+
+		:.ten_plus
+		set [z], 0x41 	; Capital A.
+		sub b, 10
+		add [z], b
+
+		:.continue
+		add i, 1
+		add z, 1	
+		ifg a, 0
 			set pc, .conv_loop_start
-		:.conv_loop_exit
 
-	set i, pop
-	set b, pop
-	set a, pop
-	set z, pop
-	set pc, pop
-
-; Converts a value to Hex ASCII output.
-; Output is 4 digits.
-; Output is 0-terminated.
-; Input
-; 	SP+1 : Buffer address to store output.
-;	SP+0 : Value to convert.
-:int_hex_conv
-	set push, z
-	set z, sp
-	add z, 2
-	set push, a
-	set push, b
-	set push, i
-	
-	set a, [z]
-	set z, [z+1]
-	; A = Value to convert.
-	; Z = Buffer address.
-
-	set i, 4
-	add z, i
-	; Z = Right most digit address.
-	; Zero-terminate string.
 	set [z], 0
+	sub z, i 	; Back to start.
+	add i, z 
+	sub i, 1 	; I = End of string.
 
-	:.conv_loop_start
+	:.invert_loop_start
+		ifg z, i
+			set pc, .exit
+		ife z, i
+			set pc, .exit
+		set a, [z]
+		set [z], [i]
+		set [i], a
+
+		add z, 1
 		sub i, 1
-		sub z, 1
-
-		set b, a
-		and b, 0xF
-
-		; Output ASCII value.
-		ifl b, 0xA
-			set pc, .no_hex
-
-		:.hex
-		set [z], 0x41
-		mod b, 10
-		set pc, .add_value
-
-		:.no_hex
-		set [z], 0x30
-
-		:.add_value
-		add [z], b
-
-		shr a, 4
-		ifg i, 0
-			set pc, .conv_loop_start
-		:.conv_loop_exit
-
+		set pc, .invert_loop_start
+	
+	:.exit
 	set i, pop
+	set c, pop
 	set b, pop
 	set a, pop
 	set z, pop
