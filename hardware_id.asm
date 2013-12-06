025b5478abf65d4c470c33ee89e41d62f708d56a
Stuart Haidon <shaidon@serayen.com>
2013-12-06 18:05:03 +0000

    Added file with defines for hardware IDs.

---
+++ hardware_id.asm
@@ -0,0 +1,23 @@
+; -----------------------
+; Title: 	Hardware Device Info Defines.
+; Author: 	Measter
+; Date:		2013/12/06
+; -----------------------
+
+; Revisions
+; 1  -  Initial release.
+
+.define SPED_ID_LEFT 		0x42BA
+.define SPED_ID_RIGHT 		0xBF3C
+
+.define FLOPPY_ID_LEFT 		0x4FD5
+.define FLOPPY_ID_RIGHT 	0x24C5
+
+.define CLOCK_ID_LEFT 		0x12D0
+.define CLOCK_ID_RIGHT 		0xB402
+
+.define KEYB_ID_LEFT 		0x30CF
+.define KEYB_ID_RIGHT 		0x7406
+
+.define LEM_ID_LEFT 		0x7349
+.define LEM_ID_RIGHT 		0xF615
